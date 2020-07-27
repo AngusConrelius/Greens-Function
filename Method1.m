c75046e221fc705e9513de36bda610b77e83fc92
AngusConrelius <68021081+AngusConrelius@users.noreply.github.com>
2020-07-27 11:48:59 -0400

    Add files via upload

--- Method1.m
+++ Method1.m
@@ -1,27 +1,44 @@
 
-k = 2;
+k = 20;
 m=5000;
-B = 1.414;
-d=1;
-M = m; 
-nu = 2;
+d=0.1;
+B = 1.414/d;
+M = m;
+nu = 1;
 K=2;
-Z=2;
-x = 1;
-y = 1;
-n = 1;
-X = x - Z;
-Y = y - n; % n is a metric tensor
+P=3.14
+p=2*P/d
+x = 0;
+y = 0.1;
+n = 0;
+z = 0;
+X = x - z;
+Y = y - n; 
 
-z = zeta(sym([0.7 i 4 11/3]));
+G=0;
 
-r = (X^2 + (Y-m*d^2))^(1/2);
-
+r = (X^2 + (Y-0*d^2))^(1/2);
+Z=k*r;
 H = besselh(nu,K,Z);
+G = G + H*exp(1)^(1i*m*B*d);
 
-for m = -M:M
-   G = H*(k*r)*exp(1)^(1i*m*B*d);
+for m = 1:M
+    r = (X^2 + (Y-m*d^2))^(1/2);
+    Z=k*r;
+    H = besselh(nu,K,Z);
+    G = G + H*exp(1)^(1i*m*B*d);
+    %   plot (m,abs(G),'+')p
+    %   hold on
+    %  pause (0.01)
+    r = (X^2 + (Y+m*d^2))^(1/2);
+    Z=k*r;
+    H = besselh(nu,K,Z);
+    G = G + H*exp(1)^(1i*m*B*d);
+    % plot (m,abs(G),'+')
+    %  hold on
+    %  pause (0.01)
 end
 
-G = (-1/4)*G
+
+Q = (-1i/4)*G
 
