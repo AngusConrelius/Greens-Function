b470e221f998014ac572ef9340e6ad6e2a01825d
AngusConrelius <68021081+AngusConrelius@users.noreply.github.com>
2020-08-10 11:28:49 -0400

    Add files via upload

--- Method1.m
+++ Method1.m
@@ -1,51 +1,69 @@
 
 k = 20;
 m = 5000;
-d = 0.1;
-B = 1.414/d;
+d = 2/20;
+
+B = sqrt(2)/d;   
+
 M = m;
-nu = 1;
+nu = 0;         
 K = 2;
-P = 3.14;
-p = 2*P/d;
-x = 0;
-y = 0.05;
-n = 0;
-z = 0;
-X = x - z;
-Y = 0.01/d;
 
-G=0;
+p = 2*pi/d;
 
-r = (X^2 + (Y-0*d)^2)^(1/2)
-Z=k*r;
-H = besselh(nu,K,Z);
-G = G + H*exp(1i*0*B*d)
+X = 0;
+Y = 0.01*d;
+G = 0;
+
+
+r = sqrt((X^2 + (Y-0*d)^2));
+Z = k*r;
+G = G + (-1i/4)*besselh(nu,K,Z)*exp(1i*0*B*d);
 %plot (o,abs(G),'+')
 %hold on
 %pause (0.01)
 
+S(1) = G;
+
 for m = 1:M
-    r = (X^2 + (Y-m*d)^2)^(1/2); %negitive m = 1 case creats r = 0 there for when used in bessel function creates a nan number
+    
+    
+    r = (X^2 + (Y-m*d)^2)^(1/2);
     Z=k*r;
-    
-    %if z ~= 0
-       %H = besselh(nu,K,Z); %avoids the z=0 casec that created NaN
-    %end
-    
-    G = G + H*exp(1i*m*B*d)
+    G = G + (-1i/4)*besselh(nu,K,Z)*exp(1i*m*B*d);
     %plot (m,abs(G),'+')
     %hold on
     %pause (0.01)
-    r = (X^2 + (Y+m*d)^2)^(1/2);
+    
+    
+    r = (X^2 + (Y-(-m)*d)^2)^(1/2);
     Z=k*r;
-    H = besselh(nu,K,Z);
-    G = G + H*exp(1i*m*B*d)
+   
+    G = G + (-1i/4)*besselh(nu,K,Z)*exp(1i*(-m)*B*d);
     %plot (m,abs(G),'+')
     %hold on
     %pause (0.01)
+    
+    S(m+1) = G;
+    
+   % if  mod(m,100) == 0
+        
+      %  plot(abs(S)); hold on
+        
+      %  plot(real(S));
+        
+       % plot(imag(S));
+        
+      %  hold off
+        
+        
+        
+      %  pause (0.01)
+        
+   % end
 end
 
+G
 
-Q = vpa(-1i/4)*G %vpa increases precision to 32 decimals instead of base 16
+
 
