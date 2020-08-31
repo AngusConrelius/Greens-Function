af836b008de94a7d68fd3dd9602ae0b9a4fb8562
AngusConrelius <68021081+AngusConrelius@users.noreply.github.com>
2020-08-31 12:22:34 -0400

    Add files via upload

---
+++ EvenSum.m
@@ -0,0 +1,75 @@
+
+function [s2L]= EvenSum(l)
+
+d = 3;
+M = 7;
+m = M;
+p = (2*pi)/d;
+B = sqrt(2)/d;
+k = 20;
+
+d = 2/20;
+
+Bm = 0;
+Ym = 0;
+nYm = 0;
+Y0 = (B^2 - k^2)^(1/2);
+theta0 = sin(1)^(-1) * (0/k); 
+
+
+Sum3 = 0;
+for m = 1:M
+    theta = sin(1)^(-1) * (Bm/k); % PDF page 11
+    Ntheta = sin(1)^(-1) * (-Bm/k);
+    Bm = B + m*p;
+    nYm = (-Bm^2 - k^2)^(1/2);
+    Ym = (Bm^2 - k^2)^(1/2);
+    
+    Sum3 = Sum3 + (exp(-2i*l*theta) / Ym*d) + (exp(2i*l*Ntheta) / nYm*d) - (((-1)^l / m*pi) * ((k / 2*m*p)^2*l))
+end
+
+z  = zeta(2*l + 1); % PDF page 11
+
+Term3 = (2i*exp(-2i*l*theta0) / Y0*d)
+
+Term4 = ((2i*(-1)^l) / pi) * ((k/2*p)^2*l) * z + (1i/l*pi) 
+
+
+
+Sum4 = 0;
+for m = 1:l
+    
+    Term5 = (l+m-1) 
+    format long
+    fact = factorial(Term5) 
+    
+    Term6 = ((-1)^m)*(2^(2*m))*fact   
+    
+    Term7 = (2*m);
+    format long
+    fact2 = factorial(Term7)
+    
+    Term8 = (l-m);
+    format long
+    fact3 = factorial(Term8)
+    
+    Term9 = fact2 * fact3
+    Term10 = Term6/Term9
+    
+    
+    Ber = bernoulli(2*m, B/p) % PDF page 16
+    
+    Term11 = ((p/k)^(2*m))*Ber;
+    Sum4 = Sum4 + (Term10 * Term11)
+    
+end
+
+
+
+ 
+s2L = Term3 - 2i*Sum3 - Term4 + (1i/pi)*Sum4
+
+end
+
+
+
