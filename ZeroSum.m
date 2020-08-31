85c15eeb99613ab30a02f6cf9886781ce88beb86
AngusConrelius <68021081+AngusConrelius@users.noreply.github.com>
2020-08-31 12:23:05 -0400

    Add files via upload

---
+++ ZeroSum.m
@@ -0,0 +1,31 @@
+
+function [S0]= ZeroSum(l)
+
+m = 7;
+k = 20;
+d = 0.1;
+B = sqrt(2)/d;
+M = m;
+c = 0.5772157; % PDF page 11
+C = 1.2020569; % PDF page 11
+p = (2*pi)/d;   % PDF page 4
+Y0 = (B^2 - k^2)^(1/2)
+Ym = 0;
+
+Sum2 = 0;
+for m = -M:M
+    if (m == 0)
+        
+        Sum2 = Sum2 
+    else
+        Bm = B + m*p % PDF page 5
+        Ym = (Bm^2 - k^2)^(1/2) % PDF page 6
+        Sum2 = Sum2 + 1/Ym - 1/p*abs(m) - (k^2 + 2*(B^2))/(2*(p^3)*abs(m)^2)
+    end
+end
+
+
+
+S0 = (-1)-(2i/pi)*(c+log(k/2*p))-(2i/Y0*d)-((2i)*(k^2 + 2*(B^2))/p^3 * d)*C - (2i/d) * Sum2
+
+end
